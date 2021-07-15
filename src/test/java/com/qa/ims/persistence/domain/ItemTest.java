356578ef6c7a49384242d2b3a29926338d5dd436
davealgie <davidalgie@hotmail.co.uk>
2021-07-15 22:24:55 +0100

    Final changes to syntax

--- src/test/java/com/qa/ims/persistence/domain/ItemTest.java
+++ src/test/java/com/qa/ims/persistence/domain/ItemTest.java
@@ -12,59 +12,59 @@ public class ItemTest {
 	
 	@Test
 	public void constructor1() {
-		Item apple = new Item("apple",1.00);
-		assertTrue(apple instanceof Item);
+		Item banana = new Item("banana",1.00);
+		assertTrue(banana instanceof Item);
 	}
 	
 	@Test
 	public void constructor2() {
-		Item apple = new Item(1l,"apple",1.00);
-		assertTrue(apple instanceof Item);
+		Item banana = new Item(1l,"banana",1.00);
+		assertTrue(banana instanceof Item);
 	}
 	
 	@Test
 	public void testGetId() {
-		Item apple = new Item(1l,"apple",1.00);
+		Item banana = new Item(1l,"banana",1.00);
 		Long expected = 1l;
-		assertEquals(expected,apple.getId());
+		assertEquals(expected,banana.getId());
 	}
 	
 	@Test
 	public void testSetId() {
-		Item apple = new Item("apple",1.00);
-		apple.setId(2l);
+		Item banana = new Item("banana",1.00);
+		banana.setId(2l);
 		Long expected = 2l;
-		assertEquals(expected,apple.getId());
+		assertEquals(expected,banana.getId());
 	}
 	
 	@Test
 	public void testGetName() {
-		Item apple = new Item("apple",1.00);
-		String expected = "apple";
-		assertEquals(expected,apple.getName());
+		Item banana = new Item("banana",1.00);
+		String expected = "banana";
+		assertEquals(expected,banana.getName());
 	}
 	
 	@Test
 	public void testSetName() {
-		Item apple = new Item("apple",1.00);
-		apple.setName("orange");
+		Item banana = new Item("banana",1.00);
+		banana.setName("orange");
 		String expected = "orange";
-		assertEquals(expected,apple.getName());
+		assertEquals(expected,banana.getName());
 	}
 	
 	@Test
 	public void testGetPrice() {
-		Item apple = new Item("apple",1.00);
+		Item banana = new Item("banana",1.00);
 		Double expected = 1.00;
-		assertEquals(expected,apple.getValue());
+		assertEquals(expected,banana.getValue());
 	}
 	
 	@Test
 	public void testSetPrice() {
-		Item apple = new Item("apple",1.00);
-		apple.setValue(4.50);
+		Item banana= new Item("banana",1.00);
+		banana.setValue(4.50);
 		Double expected = 4.50;
-		assertEquals(expected,apple.getValue());
+		assertEquals(expected,banana.getValue());
 	}
 	
 }
